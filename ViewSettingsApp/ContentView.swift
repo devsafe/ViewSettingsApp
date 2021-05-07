09fa07219e7259445aa941fb090fe03ce4edd2ff
Rafinad <rafinad@outlook.com>
2021-05-08 00:34:20 +0300

    modificators

--- ViewSettingsApp/ContentView.swift
+++ ViewSettingsApp/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{Text("Hello, SwiftUI!")
+            .font(.largeTitle)
             .padding()
+         //   .overlay(RoundedRectangle(cornerRadius: 20)
+         //               .stroke(Color.green, lineWidth: 5)
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 5)
+            .shadow(color: .red, radius: 5, x: 10, y: 10)
+        Circle()
+            .strokeBorder(Color.pink, style: StrokeStyle(lineWidth: 5, dash: [10,2,40]))
+            
+            
+            
+        }
     }
 }
 
