1536358a6afdf2b7a1497cc7f2378fa3d84e3abb
Rafinad <rafinad@outlook.com>
2021-05-08 00:47:48 +0300

    rotation3d

--- ViewSettingsApp/ContentView.swift
+++ ViewSettingsApp/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var rotation = 0.0
+    @State private var rotation2 = 0.0
     var body: some View {
         VStack{Text("Hello, SwiftUI!")
             .font(.largeTitle)
@@ -18,7 +19,12 @@ struct ContentView: View {
             .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 5)
             .shadow(color: .red, radius: 5, x: 10, y: 10)
             .rotationEffect(.degrees(rotation))
+            .rotation3DEffect(
+                .degrees(rotation2),
+                axis: (x: 0, y: 1, z: 0)
+            )
             Slider(value: $rotation, in: 0...360, step: 1.0)
+            Slider(value: $rotation2, in: 0...360, step: 1.0)
             
             
             
