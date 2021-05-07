38d5a0c5c355aae358d369337bce7da2986abfa1
Rafinad <rafinad@outlook.com>
2021-05-07 23:54:35 +0300

    Initial Commit

---
+++ ViewSettingsApp/ViewSettingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewSettingsAppApp.swift
+//  ViewSettingsApp
+//
+//  Created by Сахар Кубический on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewSettingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
